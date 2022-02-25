2d2c51aceb8e6430a06c938bb4aa38c65a491aea
Diego Rodrigues <diegokod@gmail.com>
2022-02-24 21:38:49 -0300

    JS 2v

--- scripts.js
+++ scripts.js
@@ -15,22 +15,18 @@ const Modal = {
     },
 };
 
+const Storage = {
+    get() {
+        return JSON.parse(localStorage.getItem("dev.finances:transactions")) || []
+    },
+    
+    set(transactions) {
+        localStorage.setItem("dev.finances:transactions", JSON.stringify(transactions)) 
+    }
+}
+
 const Transaction = {
-    all: [{
-        description: 'Luz',
-        amount: -50000,
-        date: '23/02/2022'
-    },
-    {
-        description: 'Website',
-        amount: 500000,
-        date: '23/02/2022'
-    },
-    {
-        description: 'Internet',
-        amount: -200000,
-        date: '23/02/2022'
-    }],
+    all: Storage.get(),
     
     add(transaction) {
         Transaction.all.push(transaction)
@@ -78,12 +74,13 @@ const DOM = {
 
     addTransaction(transaction, index) {
         const tr = document.createElement('tr')
-        tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction, index)
+        tr.dataset.index = index
 
         DOM.transactionsContainer.appendChild(tr)
     },
 
-    innerHTMLTransaction(transaction) {
+    innerHTMLTransaction(transaction, index) {
         const CSSclass = transaction.amount > 0 ? "income" : "expense"
 
         const amount = Utils.formatCurrency(transaction.amount)
@@ -94,7 +91,7 @@ const DOM = {
             <td class="${CSSclass}">${amount}</td>
             <td class="date">${transaction.date}</td>
             <td>
-              <img src="./assets/minus.svg" alt="Botão menos" />
+              <img onclick="Transaction.remove(${index})" src="./assets/minus.svg" alt="Botão menos" />
             </td>
         `
         return html
@@ -130,7 +127,7 @@ const Utils = {
     
     formatData(date) {
         
-        const splttedDate = date.splite("-")
+        const splttedDate = date.split("-")
         return `${splttedDate[2]}/${splttedDate[1]}/${splttedDate[0]}`
     },
 
@@ -180,13 +177,19 @@ const Form = {
 
         amount = Utils.formatAmount(amount)
 
-        date= Utils.formatDate(date)
+        date= Utils.formatData(date)
 
         return {
             description,
             amount,
             date
         }
+    },
+
+    clearFields() {
+        Form.description.value = ""
+        Form.amount.value = ""
+        Form.date.value = ""
     },
 
     saveTransaction() {
@@ -198,25 +201,26 @@ const Form = {
 
         try {
             Form.validateField()
-
             const transaction = Form.formatValues()
-
-            Form.saveTransaction()
-
+            Transaction.add(transaction)
+            Form.clearFields()
+            Modal.close()
+            
         } catch (error){  
             alert(error.message)
         }
     }
 }
 
+
+
 const App = {
     init() {
 
-        Transaction.all.forEach(transaction => {
-            DOM.addTransaction(transaction)
-        })
-        
+        Transaction.all.forEach(DOM.addTransaction) 
         DOM.updateBalance()
+
+        Storage.set(Transaction.all)
 
     },
     reload() {
@@ -229,9 +233,3 @@ const App = {
 App.init()
 
 
-Transaction.add({
-    id: 39,
-    description: 'ALo',
-    amount: 200,
-    date: '23/02/2021'
-})
